5324b93475282f43c12309b334c7b16a61d20a43
Jorge Soria <jorge_soriax@outlook.com>
2022-11-07 22:53:04 -0600

    feat: added Navbar component & edited theme

---
+++ src/components/Navbar.js
@@ -0,0 +1,112 @@
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { List, Moon, Sun, X } from "phosphor-react";
+import React from "react";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const links = [
+    { title: "Inspiración", href: "" },
+    { title: "Blog", href: "" },
+  ];
+
+  const MenuToggle = ({ toggle, isOpen }) => {
+    return (
+      <Box display={{ base: "block", md: "none" }} onClick={toggle}>
+        {!isOpen ? (
+          <IconButton
+            variant="ghost"
+            icon={<List size={24} weight="fill" />}
+            aria-label="Open menu"
+          />
+        ) : (
+          <IconButton
+            variant="ghost"
+            icon={<X size={24} weight="fill" />}
+            aria-label="Close menu"
+          />
+        )}
+      </Box>
+    );
+  };
+  const Links = () => {
+    return (
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        flexBasis={{ base: "100%", md: "auto" }}
+      >
+        <Stack
+          align="center"
+          justify={["center", "space-between", "flex-end", "flex-end"]}
+          direction={["column", "column", "row", "row"]}
+        >
+          <Stack spacing={4} direction={["column", "column", "row", "row"]}>
+            {links.map((link, index) => (
+              <NextLink key={index} href={link.href} passHref>
+                <Button
+                  as="a"
+                  isExternal
+                  variant="outline"
+                  w="full"
+                  fontWeight="medium"
+                >
+                  {link.title}
+                </Button>
+              </NextLink>
+            ))}
+            <Button colorScheme="brand">Hora de crear</Button>
+            <IconButton
+              onClick={toggleColorMode}
+              icon={
+                colorMode == "light" ? (
+                  <Moon size={24} weight="fill" />
+                ) : (
+                  <Sun size={24} weight="fill" />
+                )
+              }
+              aria-label="Toggle darkmode"
+            />
+          </Stack>
+        </Stack>
+      </Box>
+    );
+  };
+  const Logo = () => {
+    return <Box w="50px" h="50px" bg="black" borderRadius="full" />;
+  };
+  const NavbarContainer = ({ children }) => {
+    return (
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        w="100%"
+        py={{ base: 8, md: 12 }}
+        borderBottom={isOpen ? { base: "1px", md: 0 } : 0}
+        borderColor="gray.200"
+      >
+        {children}
+      </Flex>
+    );
+  };
+
+  return (
+    <NavbarContainer>
+      <Logo />
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <Links isOpen={isOpen} />
+    </NavbarContainer>
+  );
+}
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,34 +1,50 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  brand: {
-    50:  "#fbe9e7",
-    100: "#ffccbc",
-    200: "#ffab91",
-    300: "#ff8a65",
-    400: "#ff6f42",
-    500: "#ff5621",
-    600: "#f6501d",
-    700: "#e84918",
-    800: "#da4114",
-    900: "#c1340a",
-  },
-};
-const fonts = {
-  heading: `"Open Sans", sans-serif`,
-  body: `"Inter", sans-serif`,
-};
-const config = {
-  initialColorMode: "system",
-  useSystemColorMode: false,
-};
-const styles = {
-  global: (props) => ({
-    "html, body": {
-      color: props.colorMode === "dark" ? "white" : "#2d3748",
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#fbe9e7",
+      100: "#ffccbc",
+      200: "#ffab91",
+      300: "#ff8a65",
+      400: "#ff6f42",
+      500: "#ff5621",
+      600: "#f6501d",
+      700: "#e84918",
+      800: "#da4114",
+      900: "#c1340a",
     },
-  }),
-};
-const theme = extendTheme({ colors, fonts, config, styles });
+  },
+  fonts: {
+    heading: `"Poppins", sans-serif`,
+    body: `"Inter", sans-serif`,
+  },
+  config: {
+    initialColorMode: "system",
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: (props) => ({
+      "html, body": {
+        color: props.colorMode === "dark" ? "white" : "gray.600",
+      },
+    }),
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        borderRadius: "full",
+      },
+      sizes: {
+        md: {
+          fontSize: "md",
+          px: 10,
+          minH: '50px',
+          minW: '50px'
+        },
+      },
+    }
+  },
+});
 
 export default theme;
